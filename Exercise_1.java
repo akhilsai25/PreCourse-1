ee41434b6a9020a6385540d3c7d4e465fb7121fa
akhilsai25 <38877327+akhilsai25@users.noreply.github.com>
2025-07-09 22:00:22 -0500

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,4 +1,16 @@
-class Stack { 
+// Time Complexity :
+    // isEmpty: o(1)
+    // push: o(1)
+    // pop: o(1)
+    // peek: o(1)
+// Space Complexity :
+    // isEmpty: o(1)
+    // push: o(1)
+    // pop: o(1)
+    // peek: o(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Started with top = 0 and then realized -1 would work better
+class Stack {
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
     static final int MAX = 1000; 
@@ -7,29 +19,47 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here
+        // if the top is still -1 that specifies that array is empty
+        return top==-1;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
+        if(top==MAX) {
+            System.out.println("Stack is full");
+            return false;
+        }
         //Write your code here
+        // increment the top pointer and save the given value
+        a[++top]=x;
+        return true;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
+        if(top==-1) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
         //Write your code here
+        // Return the top element and decrement the pointer
+        return a[top--];
     } 
   
     int peek() 
-    { 
+    {
+        if(top==-1) return 0;
         //Write your code here
+        return a[top];
     } 
 } 
   
